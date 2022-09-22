4cff60d5e947dadf37ef1f65df3e045bbcacf0fb
anasusb11 <anasusb11@gmail.com>
2022-09-22 14:01:48 +0700

    update views

--- ai_simple_library/__manifest__.py
+++ ai_simple_library/__manifest__.py
@@ -20,14 +20,14 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'product'],
+    'depends': ['base', 'product', 'sale', 'account'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'views/anggota.xml',
+        # 'views/anggota.xml',
         'views/menuitems.xml',
         # 'views/buku_tersedia',
         # 'views/buku_tersewa'
