67682e8b51c76dda6f822bf21c3f9b4b953e0d65
anasusb11 <anasusb11@gmail.com>
2022-09-22 12:14:48 +0700

    update view buku

--- ai_simple_library/models/models.py
+++ ai_simple_library/models/models.py
@@ -1,9 +1,22 @@
 from odoo import models, fields, api
 
 
+
+class ProductProduct(models.Model): 
+    _inherit ='product.product'
+
+    isbn = fields.Char('ISBN Code', help="Shows International Standard Book Number")
+    catalog_num = fields.Char('Catalog Number', help="Shows Identification number of books")
+    author_id = fields.Many2one('res.partner', 'Author')
+    publisher_id = fields.Many2one('res.partner', 'Publisher')
+    nbpage = fields.Integer('Number of Pages')
+    location_id = fields.Many2one('stock.location', 'Location', help="Shows position of book")
+    num_edition = fields.Integer('No. Edition', help="Edition number of book")
+    resensi = fields.Text('Resensi')
+    state = fields.Selection([('available', 'Available'), ('rent', 'Rented')], 'State', readonly=True, default='available')
+
 class ResPartner(models.Model):
     _inherit = 'res.partner'
-    
     
     penulis = fields.Boolean(string= 'Penulis ?')
     anggota = fields.Boolean(string='Anggota ?')
